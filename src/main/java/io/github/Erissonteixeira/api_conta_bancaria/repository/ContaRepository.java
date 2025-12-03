462f53a1eb674aaf130005e21c765876f641c891
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-03 15:23:17 -0300

    refactor(repository): adicionar anotação @Repository ao ContaRepository

--- src/main/java/io/github/Erissonteixeira/api_conta_bancaria/repository/ContaRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_conta_bancaria/repository/ContaRepository.java
@@ -2,6 +2,8 @@ package io.github.Erissonteixeira.api_conta_bancaria.repository;
 
 import io.github.Erissonteixeira.api_conta_bancaria.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContaRepository extends JpaRepository<Conta, Long>{
 }
