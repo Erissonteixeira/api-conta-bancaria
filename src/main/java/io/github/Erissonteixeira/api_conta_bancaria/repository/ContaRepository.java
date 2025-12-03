3974cbee2c0f60d123a4ae818d7c07624f52159b
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-03 15:21:00 -0300

    refactor(repository): corrigir nome de ContraRepository para ContaRepository

--- src/main/java/io/github/Erissonteixeira/api_conta_bancaria/repository/ContaRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_conta_bancaria/repository/ContaRepository.java
@@ -3,5 +3,5 @@ package io.github.Erissonteixeira.api_conta_bancaria.repository;
 import io.github.Erissonteixeira.api_conta_bancaria.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContraRepository extends JpaRepository<Conta, Long>{
+public interface ContaRepository extends JpaRepository<Conta, Long>{
 }
