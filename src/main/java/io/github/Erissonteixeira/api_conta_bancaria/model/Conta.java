2aed62034893fdb046d68099943efb578da4d531
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-03 14:35:51 -0300

    feat(model): adicionar entidade Conta com id, titular e saldo

---
+++ src/main/java/io/github/Erissonteixeira/api_conta_bancaria/model/Conta.java
@@ -0,0 +1,43 @@
+package io.github.Erissonteixeira.api_conta_bancaria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Conta{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titular;
+    private double saldo;
+
+    public Conta(){
+    }
+
+    public Conta(String titular, double saldo){
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitular(){
+        return titular;
+    }
+
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+}
