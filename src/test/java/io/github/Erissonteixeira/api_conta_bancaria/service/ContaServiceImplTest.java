7df9673e2f2eac70fadb4e0aa3360614cededbb4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-04 19:59:24 -0300

    test(service): cria teste deveListarTodasAsContasComSucesso validando findAll e mapeamento para DTO

--- src/test/java/io/github/Erissonteixeira/api_conta_bancaria/service/ContaServiceImplTest.java
+++ src/test/java/io/github/Erissonteixeira/api_conta_bancaria/service/ContaServiceImplTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -110,6 +112,29 @@ public class ContaServiceImplTest{
             return;
         }
         throw new AssertionError("Era esperada uma RuntimeException, mas nada foi lançado");
+    }
+    @Test
+    void deveListarTodasAsContasComSucesso(){
+        Conta conta1 = new Conta("Erisson", 1000.0);
+        Conta conta2 = new Conta("Teixeira", 2000.0);
+
+        try {
+            var field = Conta.class.getDeclaredField("id");
+            field.setAccessible(true);
+            field.set(conta1, 1l);
+            field.set(conta2, 2l);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        List<Conta> contasMock = List.of(conta1, conta2);
+
+        when(contaRepository.findAll()).thenReturn(contasMock);
+
+        List<ContaResponseDto> resposta = contaService.listar();
+
+        assertEquals(2, resposta.size());
+        assertEquals("Erisson", resposta.get(0).titular());
+        assertEquals("Teixeira", resposta.get(1).titular());
     }
 }
 
