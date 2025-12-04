c104d2f5cd694118ed9ff6e21d72199c229252b9
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-04 19:33:13 -0300

    feat(service): adiciona assinatura do método listar() retornando lista de ContaResponseDto

--- src/main/java/io/github/Erissonteixeira/api_conta_bancaria/service/ContaService.java
+++ src/main/java/io/github/Erissonteixeira/api_conta_bancaria/service/ContaService.java
@@ -3,10 +3,13 @@ package io.github.Erissonteixeira.api_conta_bancaria.service;
 import io.github.Erissonteixeira.api_conta_bancaria.dto.ContaRequestDto;
 import io.github.Erissonteixeira.api_conta_bancaria.dto.ContaResponseDto;
 
+import java.util.List;
+
 public interface ContaService{
     ContaResponseDto criarConta(ContaRequestDto dto);
     ContaResponseDto buscarPorId(Long id);
     ContaResponseDto atualizar(Long id, ContaRequestDto dto);
+    List<ContaResponseDto> listar();
 
 
 }
