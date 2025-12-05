c9967a112f4585a345b6428a0bb2d89fb4b9fef8
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-05 12:00:13 -0300

    feat(controller): implementa método criar() recebendo ContaRequestDto e delegando criação ao service

---
+++ src/main/java/io/github/Erissonteixeira/api_conta_bancaria/controller/ContaController.java
@@ -0,0 +1,22 @@
+package io.github.Erissonteixeira.api_conta_bancaria.controller;
+
+import io.github.Erissonteixeira.api_conta_bancaria.dto.ContaRequestDto;
+import io.github.Erissonteixeira.api_conta_bancaria.dto.ContaResponseDto;
+import io.github.Erissonteixeira.api_conta_bancaria.service.ContaService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/contas")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    public ContaResponseDto criar(@RequestBody ContaRequestDto dto){
+        return contaService.criarConta(dto);
+    }
+}
